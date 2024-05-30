9c560514fd47a6276b4d9a602227f8a1b06d0247
Taavi Tammaru <taavi1p@gmail.com>
2024-05-30 22:36:59 +0300

    cv better

--- src/components/cv.js
+++ src/components/cv.js
@@ -1,16 +1,22 @@
-// ResumePage.js
 import React from 'react';
 import '../styles/cv.css';
-import resumeImage from '../images/resume.jpg'; // Make sure to replace this with the actual path to your image
+import resumeImage from '../images/resume.jpg';
+
 
 const ResumePage = () => {
+  const openPdf = () => {
+    window.open('C:/Users/Gebruiker/Desktop/website/my-website/public/resume.pdf', '_blank', 'noopener,noreferrer');
+  };
   return (
     <div className="resume-page">
       <img src={resumeImage} alt="Resume" className="resume-image" />
+      <div className="pdf-link">
+      <button onClick={openPdf}>
+        Open PDF
+      </button>
+    </div>
     </div>
   );
 }
 
 export default ResumePage;
-
-
