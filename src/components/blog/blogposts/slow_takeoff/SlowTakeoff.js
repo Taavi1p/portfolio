08a899e3e07f43351cbb26afed01b8573b718638
Taavi Tammaru <42722494+Taavi1p@users.noreply.github.com>
2026-02-08 19:02:01 +0200

    blog post

--- src/components/blog/blogposts/slow_takeoff/SlowTakeoff.js
+++ src/components/blog/blogposts/slow_takeoff/SlowTakeoff.js
@@ -7,43 +7,43 @@ return (
 
 <BlogPost
           title="The Slow Takeoff Scenario"
-          date="October 5, 2025"
+          date="February 6, 2026"
           content={[
             <p key="p1">
              There is a solid argument that superhuman AI could arrive more gradually than expected and actually be more than 25 years away. 
              Last year I wrote a post where I emphasized that AI progress could get out of hand quickly, since then I have gathered evidence 
-             of the opposite. This standpoint comes from debunking common misconceptions held in the AI community and is supported by recent 
+             of the opposite. This standpoint comes from debunking some common misconceptions held in the AI community and is supported by recent 
              empirical evidence.
             </p>,
             <p key="p2">
               First of all, why is it important to think about possible AI timelines? No matter how or when it arrives, no doubt superhuman 
               AI will have grand impacts on the economy. So timing those impacts is crucial for making effective career decisions. Entering 
               a field that could become irrelevant in the next decade or investing in an idea that will pay fruit only after 40 years could 
-              become costly. Eliminating this uncertainty would make for better decisionmaking all around.
+              be costly. Eliminating this uncertainty would make for better decisionmaking all around.
             </p>,
             <p key="p3">
-             Scaling up the current technology is having diminishing returns. We can’t expect progress only by scaling up the existing LLMs,
-              this was most pronounced by GPT-5 being only an incremental improvement on GPT-4. The LLMs do have a ceiling and some of the 
-              initial limitations persist through the models. It is impressive in its wide knowledgebase but doesn't have novel insights, 
+             Scaling up the current technology is having diminishing returns. We can’t expect continued progress only by scaling up the existing LLMs,
+              this was most pronounced by GPT-5 being only an incremental improvement compared to GPT-4. The LLMs do have a ceiling and some of the 
+              initial limitations persist through the models. The current LLMs are impressive in their wide knowledgebase but don't have novel insights, 
               multimodality, long term agency, and situational awareness - all the things we expect from a human. To unlock these 
-              capabilities a completely new approach is needed.
+              capabilities likely a completely new approach is needed.
 
             </p>,
             <p key="p4">
              Most jobs are actually harder than people from the outside perceive. Therefore the automation of human jobs could be much 
              slower than anticipated. We often like to frame a job by the one central task but in reality almost all jobs encompass far 
              more than a singular task. For example the general public might think of an accountant as someone who just inserts numbers 
-             into excel and does some simple arithmetic calculations - a task an AI could easily do now. But in fact the job consists of 
+             into excel and does some simple arithmetic calculations - a task an AI can easily do now. But in fact the job consists of 
              so much more subtleties like reading the emotions of people, interpreting the true meaning and intentions behind a coworkers' 
              words, catching conceptual errors, interpreting meaning and consequences of the numbers and voicing these in the appropriate 
-             manner to the managers. The combination of these soft skills is absolutely necessary for an accountant and is currently far 
+             manner to the managers and so on. The combination of these soft skills is absolutely necessary for an accountant and is currently far 
              out of AI’s reach. 
             </p>,
 
             <p key="p5">
             A human perception of ability often makes AI appear more impressive than it is. For humans writing a great essay or solving a 
             complex math problem is strongly correlated with other advanced abilities and as well as all the basic human abilities. So 
-            when we see a strong intellectual performance in one domain we instinctively assume many other abilities from that person but 
+            when we see a strong intellectual performance in one domain we instinctively infer many other abilities from that person but 
             this is not relevant for AI. For an AI excellent performance in one domain is not correlated with more basic human capabilities. 
             Therefore this common human fallacy makes AI seem much more capable than it actually is. 
             </p>,
@@ -57,7 +57,7 @@ return (
             </p>,
             
             <p key="p7">
-            Why the described ‘intelligence explosion’ wouldn’t necessarily have huge consequences as often portrayed? A boost in intelligence
+            Another misconception seems to be the often portrayed huge consequences of the ‘intelligence explosion’. A boost in intelligence
              alone wouldn’t amount to much. It is necessary to build structure around the increased intelligence to effectively cultivate it. 
              Plugging greater intelligence into the current system would be woefully inefficient. Humans have had the same amount of brainpower 
              for hundreds of thousands of years and yet only in the last hundreds of years we started making significant progress on how to 
@@ -74,16 +74,21 @@ return (
             
             <p key="p9">
             When considering these nuances, when would be the expected arrival of superhuman intelligence?  It is difficult to assign any date to 
-            it with any considerable confidence. I would say that based on the information we have the lower bound is much further than 2035 as 
-            stated in my previous post. I would venture to guess 2050 at the earliest.
+            it with any considerable confidence. I would say that based on the information we have the lower bound is much further out than 2029 as 
+            stated in my previous post. If I had to I would venture a guess for the lower bound at 2050.
             </p>,
 
             <p key="p10">
-            Saying all of this I still feel like it is inevitable that superintelligence will be a reality at some point in the future. I don't
-            think that it will be smooth linear or exponential progress from now on, nor do I believe in the intelligence explosion where at a 
-            certain point the agent goes into perfect recursive self development. But I don't think there are any fundamental limiting factors 
+            Saying all of this I still feel like it is inevitable that superintelligence will be here at some point. I don't
+            think that it will be a smooth linear or exponential progress from now on, nor do I believe in the intelligence explosion where at a 
+            certain point the agent goes into perfect recursive self development. Regardless, I don't think there are any fundamental limiting factors 
             to arrive at superhuman intelligence - if we can simulate a human brain we can simulate a super human brain. 
             </p>,
+
+            <p key="p11">
+              <i>Inspired by the Tamay Besiroglu and Ege Erdil podcast</i>
+            </p>,
+
               ]}
         />
             )
